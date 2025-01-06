7a99689fa71875e6cf886a5339a1881653e1a664
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-01-06 09:26:35 +0100

    Added exception handling: no such product

--- manager-app/src/main/java/ag/shop/manager/controller/ProductController.java
+++ manager-app/src/main/java/ag/shop/manager/controller/ProductController.java
@@ -3,9 +3,14 @@ package ag.shop.manager.controller;
 import ag.shop.manager.controller.payload.UpdateProductPayload;
 import ag.shop.manager.entity.Product;
 import ag.shop.manager.service.ProductService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("catalogue/products/{productId:\\d+}")
@@ -21,7 +26,7 @@ public class ProductController {
 
     @GetMapping
     public String getProduct() {
-        return "catalogue/products/product";
+        return "catalogue/products/products";
     }
 
     @GetMapping("edit")
@@ -39,5 +44,13 @@ public class ProductController {
     public String deleteProduct(@ModelAttribute("product") Product product) {
         this.productService.deleteProduct(product.getId());
         return "redirect:/catalogue/products/list";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(NoSuchElementException exception, Model model,
+                                               HttpServletResponse response) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        model.addAttribute("error", exception.getMessage());
+        return "errors/404";
     }
 }
