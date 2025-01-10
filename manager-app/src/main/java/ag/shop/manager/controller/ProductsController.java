7327c77d9d62e2b118418b0e5d7d6b80a4f5490c
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-01-10 15:03:59 +0100

    Clean up code

--- manager-app/src/main/java/ag/shop/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ag/shop/manager/controller/ProductsController.java
@@ -7,8 +7,6 @@ import ag.shop.manager.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
