9440e70bbcad737b582076d2157b321dcdcdeb45
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-01-13 17:39:05 +0100

    Product filtration added

--- catalogue-service/src/main/java/ag/shop/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ag/shop/catalogue/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package ag.shop.catalogue.repository;
 
 import ag.shop.catalogue.entity.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
+    @Query(value = "select * from catalogue.t_product where c_title ilike :filter", nativeQuery = true)
+    Iterable<Product> findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
--- manager-app/src/main/java/ag/shop/manager/controller/ProductsController.java
+++ manager-app/src/main/java/ag/shop/manager/controller/ProductsController.java
@@ -17,8 +17,9 @@ public class ProductsController {
     private final ProductsRestClient productsRestClient;
 
     @GetMapping("list")
-    public String getProductList(Model model) {
-        model.addAttribute("products", this.productsRestClient.findAllProducts());
+    public String getProductList(Model model, @RequestParam(name = "filter", required = false) String filter) {
+        model.addAttribute("products", this.productsRestClient.findAllProducts(filter));
+        model.addAttribute("filter");
         return "catalogue/products/list";
     }
 
