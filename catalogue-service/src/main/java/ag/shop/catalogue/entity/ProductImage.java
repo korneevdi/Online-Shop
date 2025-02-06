9ab82c094ab7533b3ba1d4fc4510df2b331937ea
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-02-06 18:13:20 +0100

    Fixed bug with (de)serialization

--- catalogue-service/src/main/java/ag/shop/catalogue/entity/ProductImage.java
+++ catalogue-service/src/main/java/ag/shop/catalogue/entity/ProductImage.java
@@ -1,23 +1,27 @@
 package ag.shop.catalogue.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(schema = "catalogue", name = "t_product_image")
+@ToString(exclude = "product")
 public class ProductImage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
+    @JsonBackReference
     private Product product; // Connection to the product
 
     @Column(name = "image_url", nullable = false)
